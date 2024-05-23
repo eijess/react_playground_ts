e23c12a40371cc86099788e86750198a6a19db18
Jessica <jessica_costaignacio@hotmail.com>
2024-05-23 00:31:42 -0300

    Conceitos básicos de React

---
+++ src/pages/contador/Contador.tsx
@@ -0,0 +1,25 @@
+﻿import { useEffect, useState } from "react"
+
+function Contador() {
+  
+  const[valor, setValor] = useState(0); // O estado se chama valor, a função que muda o estado se chama setValor
+  
+  function handleClick(){
+    setValor(valor + 1); //chamando a função set responsável por mudar o estado, e pedindo pra ela pegar o valor atual e mudar adicionando 1
+  }
+  // console.log(valor); não deixar aparente na produção para não expor dados da aplicação no navegador
+
+  return (
+    <div>
+      
+      <h2>Componente Contador</h2>
+
+      <p> O valor atual é: {valor}</p>
+
+      <button onClick={handleClick}>Adicionar 1</button>
+
+    </div>
+  )
+}
+
+export default Contador
---
+++ src/pages/tarefa/Tarefa.tsx
@@ -0,0 +1,33 @@
+﻿import { useEffect, useState } from "react"
+
+function Tarefa() {
+
+  const [completed, setCompleted] = useState(false); /*criei uma função aônima (uma arrow function) dentro do evento onClick para usar o setCompleted sem precisar criar a função aqui em cima*/
+  const [tarefa, setTarefa] = useState('');
+
+  useEffect(() => {
+    if (completed) {
+      setTarefa('Parabéns, tarefa concluída!')
+    }else{
+      setTarefa('')
+    }
+  }, [completed]) //o useEffet só será executado se tiver uma alteração de estado no estado completed
+
+  // console.log("Completed: " + completed);
+
+  return (
+    <div>
+      
+      <h2>Componente Tarefa</h2>
+
+      <h3>{tarefa}</h3>
+
+      <p>Conclua a tarefa</p>
+
+      <button onClick={() => setCompleted(!completed)}>Concluir Tarefa</button> 
+      
+      </div>
+  )
+}
+
+export default Tarefa
